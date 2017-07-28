a88020adcd54e5c105d6b2fc2183c8b298583028
Siddharth Goyal <siddharth.game@gmail.com>
2017-07-29 00:26:58 +0530

    production

--- trydjango19/wsgi.py
+++ trydjango19/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trydjango19.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trydjango19.settings.production")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
