79fc5ef0f5e6aa32c8f4d6177f5ea397176b630f
Siddharth Goyal <siddharth.game@gmail.com>
2017-07-30 23:14:28 +0530

    Profile site made

--- profilesite/urls.py
+++ profilesite/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+#    url(r'^$', views.index),
+	 url(r'^$', TemplateView.as_view(template_name='home_page.html')),
 ]
